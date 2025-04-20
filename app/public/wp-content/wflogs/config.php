06f0f534551639ddfbd598c83b64a9753a55d584
Next-25 <live.with.WT19i@gmail.com>
2025-04-20 16:38:33 +0700

    full site, login managment

---
+++ app/public/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1745738909;s:7:"authKey";s:64:"V[Ej k8Ds79009y0f|oS?{Tq&O;jf/!$U=@^Inp7+1TY{df>4NJ(0$+*7j>p3KV{";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:2043;}
